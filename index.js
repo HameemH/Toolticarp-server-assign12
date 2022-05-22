c2a6cc13e5bbca0628426d09657812a2e430077a
HameemH <hameemhossain2@gmail.com>
2022-05-22 21:32:36 +0600

    products query by id api created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -23,12 +23,18 @@ async function run(){
             const products = await cursor.toArray();
             res.send(products);
           });
+          app.get('/products/:id', async(req,res) =>{
+            const id = req.params.id;
+            const query ={_id: ObjectId(id)};
+            const result = await ProductsCollection.findOne(query);
+            res.send(result)
+        })
     }
     finally{
 
     }
 }
-run().catch(console.dir())
+run().catch(console.dir)
 app.get('/', (req, res) => {
     res.send('Hello From manufacturer!')
   })
