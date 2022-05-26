fc4ef9a680bdcbdf1106900d5fb009475e785feb
HameemH <hameemhossain2@gmail.com>
2022-05-26 20:59:22 +0600

    testing api

--- index.js
+++ index.js
@@ -62,6 +62,13 @@ async function run(){
           const orders = await cursor.toArray();
           res.send(orders);
         });
+        app.get('/orders', async (req, res) => {
+          
+          const query = {}
+          const cursor = ordersCollection.find(query);
+          const orders = await cursor.toArray();
+          res.send(orders);
+        });
         app.delete('/orders/:id', async(req, res) =>{
           const id = req.params.id;
           const query = {_id: ObjectId(id)};
