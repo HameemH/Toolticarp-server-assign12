8763f6ae9c7fd5890825e3de2a8cdc10ca86d8c6
HameemH <hameemhossain2@gmail.com>
2022-05-26 21:34:22 +0600

    cors origin prb testing solution

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin:true,
+  optionsSuccessStatus:200,
+  credentials:true
+}));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.stwgp.mongodb.net/?retryWrites=true&w=majority`;
