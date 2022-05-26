0e52977e8142c0d2758bb291b0dae8c4f26d09e6
HameemH <hameemhossain2@gmail.com>
2022-05-26 21:09:46 +0600

    testing

--- index.js
+++ index.js
@@ -52,7 +52,13 @@ async function run(){
             res.send(addOrder)
 
         })
-        
+        app.get('/orders', async (req, res) => {
+          
+          const query = {}
+          const cursor = ordersCollection.find(query);
+          const orders = await cursor.toArray();
+          res.send(orders);
+        });
        
         app.get('/orders', async (req, res) => {
           const email = req.query.email;
@@ -62,13 +68,7 @@ async function run(){
           const orders = await cursor.toArray();
           res.send(orders);
         });
-        app.get('/orders', async (req, res) => {
-          
-          const query = {}
-          const cursor = ordersCollection.find(query);
-          const orders = await cursor.toArray();
-          res.send(orders);
-        });
+     
         app.delete('/orders/:id', async(req, res) =>{
           const id = req.params.id;
           const query = {_id: ObjectId(id)};
