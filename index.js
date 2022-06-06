789b4e7a3bf52609d443f4e06da40dd2fcad5b60
HameemH <hameemhossain2@gmail.com>
2022-06-07 01:38:34 +0600

    solving

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 // }));
 // app.use(express.json());
 const corsConfig = {
-  origin: '*',
+  origin: 'http://localhost:3000/',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 }
